4701414db79182bb2f831b912873814a013a9522
du4493 <du4493@gmail.com>
2016-07-14 14:00:33 -0400

    Update R_kata.r

--- R_kata.r
+++ R_kata.r
@@ -4,15 +4,15 @@ input_vectors <- read.table("input.txt", header=F, fill=T)
 #create a vector containing the means of the columns, ignores NA values, incase vectors of differing lengths are provided
 mean <- colMeans(input_vectors, na.rm=T)
 
-#create a vector to output, contains the means in case all of the conditionals are false
+#create a vector to output
 label <- mean
 
-#creates boolean vectors identifying which means satisfy the required conditions
+#creates logical vectors identifying which means satisfy the required conditions
 three_selector <- mean%%3==0
 five_selector <- mean%%5==0
 fifteen_selector <- mean%%15==0
 
-#changes values in label as selected above
+#changes values in label as needed 
 label[three_selector] <- "word"
 label[five_selector] <- "smith"
 label[fifteen_selector] <- "wordsmith"
