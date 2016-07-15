9f6ca0040df597c4df608b7650cc5c92090caec7
du4493 <du4493@gmail.com>
2016-07-14 20:33:25 -0400

    Update test_1.r

--- tests/testthat/test_1.r
+++ tests/testthat/test_1.r
@@ -1,2 +1,7 @@
-library()
-context()
+library(RkataPackage)
+context("check_and_label")
+test_that("Selectors work as expected...", {
+  expect_match(check_and_label(3),"word")
+  expect_match(check_and_label(5),"smith")
+  expect_match(check_and_label(15),"wordsmith")
+})
